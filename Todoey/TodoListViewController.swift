8c40e183e2574fb95ff3b45cc8ad9ffb72d6c3af
Locke-and-Owl <christian.locklair@gmail.com>
2019-07-15 23:17:51 +0200

    Todo list complete with datasource and delegate methods.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Christian Locklair on 14.07.19.
+//  Copyright © 2019 Christian Locklair. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogoron"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK - Tableview Datasource Methods
+    // numberOfRowsInSection und cellForRowAt die zwei wichtigsten Tableview Methods!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    
+    //WICHTIG: Tableview Method die gecalled wird, wenn eine Tableview Cell geklickt wird
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
+
